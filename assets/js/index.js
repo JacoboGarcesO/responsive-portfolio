0d1acb6aafcf32f80b46f7dd6e2eef92b9e84d33
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2022-01-19 07:13:02 -0500

    Skills section was finisged

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,23 +3,38 @@ const init = () => {
   const navOpenButton = document.querySelector('.header__open');
   const navCloseButton = document.querySelector('.header__close');
   const navMenuLinks = document.querySelectorAll('.header__link');
+  const skillsContent = document.querySelectorAll('.skills__content');
+  const skillsHeader = document.querySelectorAll('.skills__header');
 
-  navOpenButton.addEventListener('click', addClass.bind(this, navMenu, 'header__menu--active'));
-  navCloseButton.addEventListener('click', removeClass.bind(this, navMenu, 'header__menu--active'));
+  navOpenButton.addEventListener('click', addClass.bind(this, 'header__menu--active', navMenu));
+  navCloseButton.addEventListener('click', removeClass.bind(this, 'header__menu--active', navMenu));
 
   navMenuLinks.forEach(handleClickNavLink.bind(this, navMenu));
+
+  skillsHeader.forEach(handleClickSkill.bind(this, skillsContent));
 }
 
-const addClass = (element, className) => {
+const addClass = (className, element) => {
   element.classList.add(className);
 }
 
-const removeClass = (element, className) => {
+const removeClass = (className, element) => {
   element.classList.remove(className);
 }
 
 const handleClickNavLink = (menu, link) => {
-  link.addEventListener('click', removeClass.bind(this, menu, 'header__menu--active'));
+  link.addEventListener('click', removeClass.bind(this, 'header__menu--active', menu));
+}
+
+const handleClickSkill = (skillsContent, skill) => {
+  skill.addEventListener('click', toggleSkillContent.bind(this, skillsContent, skill));
+}
+
+const toggleSkillContent = (skillsContent, skill) => {
+  skillsContent.forEach(removeClass.bind(this, 'skills__open'));
+  skillsContent.forEach(addClass.bind(this, 'skills__close'));
+  addClass('skills__open', skill.parentNode);
+  removeClass('skills__close', skill.parentNode);
 }
 
 addEventListener('load', init);
